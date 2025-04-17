76ab0fd2d795f39027849ebf2492964142634d80
benearne <b.arnold@posteo.de>
2025-04-17 15:00:02 +0200

    add hover effect

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const container = document.getElementById('container');
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement('div');
         square.classList.add('square');
+
+        // Hover effect 
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hovered');
+        });
+
         container.appendChild(square);
 }
